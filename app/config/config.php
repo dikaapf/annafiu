2401c1de83e60e27ce814da0c0cdca756c474aba
dikaapf <dikaapf@gmail.com>
2023-05-01 15:07:50 +0000

    update gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 //$config['base_url'] = BASE_URL;
-$config['base_url'] = 'http://localhost/simasjid';
+$config['base_url'] = 'http://kel2.remajacemput.pe.hu';
 /*
 |--------------------------------------------------------------------------
 | Index File
